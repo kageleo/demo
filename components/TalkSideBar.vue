f4f1e2a9144efa863c611fe2175322ec762ada25
kageleo <60954189+kageleo@users.noreply.github.com>
2023-05-30 03:22:03 +0900

    Update TalkSideBar.vue

--- components/TalkSideBar.vue
+++ components/TalkSideBar.vue
@@ -1,12 +1,17 @@
 <template>
-    <div id="talk-side-bar">
+    <div id="talk-side-bar" @click="$emit('toggleF')">
         <ul>
             <li>
-                <div>
+                <div class="menbers">
                     <img src="../../images/人型アイコン.png" width="55px" height="55px">
-                    <div>
+                    <div class="talk-div">
                         <p class="talk-title">牛久 朝之</p>
-                        <p class="talk-msg"></p>
+                        <div class="pp">
+                            <p class="talk-msg">{{ newMsg[0] }}</p>
+                        </div>
+                        <div class="pp pp-date">
+                            <p class="talk-date">{{ newMsg[1] }}</p>
+                        </div>
                     </div>
                 </div>
             </li>
@@ -16,13 +21,16 @@
 
 <script>
 export default {
-    name: 'TalkSideBar'
+    name: 'TalkSideBar',
+    props: {
+        newMsg: []
+    }
 }
 </script>
 
 <style scoped>
 #talk-side-bar {
-    width: 300px;
+    width: 500px;
     height: calc(100vh - 100px);
     background-color: rgb(33, 62, 100);
 }
@@ -51,6 +59,10 @@ img {
     margin: 2px 2px 0 3px;
 }
 
+.talk-div {
+    text-align: left;
+}
+
 p {
     margin: 0;
     line-height: 1;
@@ -63,7 +75,30 @@ p {
 .talk-msg {
     color: gray;
     margin-top: 2px;
+    font-size: 12px;
+    white-space: pre-wrap;
+    position: absolute;
+    top: 26px;
+    line-height: 1.2;
+    width: 200px;
+}
+
+.talk-date {
+    color: gray;
+    margin-top: 2px;
     font-size: 10px;
+    width: 90px;
+    position: absolute;
+    right: 0;
+    bottom: 27px;
+}
+
+.pp {
+    display: inline-block;
+}
+
+.menbers {
+    position: relative;
 }
 
 </style>
