5ef3e3495d7b610655aa8edc7e501af022ef7c9c
kageleo <60954189+kageleo@users.noreply.github.com>
2023-05-29 18:01:11 +0900

    Update TalkSideBar.vue

--- components/TalkSideBar.vue
+++ components/TalkSideBar.vue
@@ -1,37 +1,69 @@
 <template>
-    <div class="talk-side-bar">
+    <div id="talk-side-bar">
         <ul>
-            <li>上司</li>
+            <li>
+                <div>
+                    <img src="../../images/人型アイコン.png" width="55px" height="55px">
+                    <div>
+                        <p class="talk-title">牛久 朝之</p>
+                        <p class="talk-msg"></p>
+                    </div>
+                </div>
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
-
 export default {
-    name: "TalkSideBar",
+    name: 'TalkSideBar'
 }
 </script>
 
 <style scoped>
-
-.talk-side-bar {
-    display: inline-block;
-    background-color: aqua;
+#talk-side-bar {
     width: 300px;
     height: calc(100vh - 100px);
+    background-color: rgb(33, 62, 100);
 }
 
-.talk-side-bar > ul {
-    margin: 0;
+#talk-side-bar > ul {
     padding-left: 0;
     text-align: center;
+    margin: 0;
 }
 
-.talk-side-bar > ul > li {
+li {
     list-style: none;
-    height: 50px;
+    height: 60px;
+    color: black;
+    font-size: 17px;
     line-height: 3;
+    background-color:  white;
+}
+
+li > div {
+    display: flex;
+}
+
+img {
+    display: inline-block;
+    margin: 2px 2px 0 3px;
+}
+
+p {
+    margin: 0;
+    line-height: 1;
+}
+
+.talk-title {
+    margin-top: 8px;
+}
+
+.talk-msg {
+    color: gray;
+    margin-top: 2px;
+    font-size: 10px;
 }
 
 </style>
